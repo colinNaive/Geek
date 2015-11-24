6fd6a2624f077b4e104b1d45300df1cd8aaeafcc
colinNaive <colinNaive@shu.edu.cn>
2015-11-24 10:02:14 +0800

    smartDevice

---
+++ smartDevice/js/shan.js
@@ -0,0 +1,452 @@
+var settingClickFlag=1;
+var canClick=true;//允许点击
+var ws;
+$(function(){
+    //添加设备列表
+    addDeviceList('apply_w');
+    //websocket连接
+	connectWebsocket();
+    //延时弹窗隐藏
+    $('#mybg').hide();
+	$li1 = $(".apply_nav .apply_array");
+	$window1 = $(".apply .apply_w");
+	$left1 = $(".apply .img_l");
+	$right1 = $(".apply .img_r");
+	$time_set= $("#time_set");
+	$power_protect= $("#power_protect");
+	$window1.css("width", $li1.length*8.4+"rem");
+	var clickEvent="ontouchstart" in document.documentElement ? "touchstart" :"click";
+	var myAlert = document.getElementById("alert"); 
+	$('.setting_menu .div_img_setting_then').css('display','none');
+	$('.edit').css("display","none");
+	//默认隐藏
+	$('.box_input').hide();
+	$('.box').hide();
+	$('.extend_time').hide();
+	var time_extend=1,time_set=1,power_protect=1;
+	//插座按钮状态
+	var state = false;
+	var output = 'CONTROL ' + mac + ' P ' + passWord + ' AT+TIMETASK=1,';
+	var editpass;
+	var lc1 = currentIndex;
+	var rc1 = $li1.length-1-currentIndex;
+	var currentLocation=0;
+	/*******插座部分控制*********/
+	var deviceList=$('.apply_array .apply_img img');
+	//初始化设备mac、note、password、type
+	mac=mac_[currentIndex].trim();
+	note = note_[currentIndex];
+	passWord = password_[currentIndex].trim();
+	type = type_[currentIndex].trim();
+	lampBottomDisplay();//彩灯颜色滚动条和颜色选择块的显示控制
+	console.log("type="+type);
+	//初始化当前设备位置
+	currentLocation= 8.4*currentIndex;
+	$window1.animate({left:'-='+currentLocation+'rem'}, 1);
+	for(var i=0;i<deviceList.length;i++){
+		deviceList[i].id="No" + i;
+	}
+	/*******插座部分控制*********/
+	deviceList.click(function(){
+		if(!canClick){
+			return;
+		}
+		if(state){
+			SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+CLOSE=1\r\n'));
+			$("#No"+currentIndex).attr('src',"imgs/plug_off.png"); 
+			state = false;
+		}else{
+			SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+OPEN=1\r\n'));
+			$('#No'+currentIndex).attr('src',"imgs/plug.png"); 
+			state = true;
+		}
+	});
+	//左右位置移动
+	$left1.click(function(){
+		if (lc1 < 1) {
+			alert("已经是第一台设备");
+			return;
+		}
+		lc1--;
+		rc1++;
+		$window1.animate({left:'+=8.4rem'}, 500);
+		currentIndex--;
+		mac=mac_[currentIndex].trim();
+		note = note_[currentIndex];
+		type = type_[currentIndex];
+		console.log("left_type="+type);
+		passWord = password_[currentIndex].trim();
+		SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+NODE=1\r\n'));
+		lampBottomDisplay();//彩灯颜色滚动条和颜色选择块的显示控制
+	});
+
+	$right1.click(function(){
+		
+		if (rc1 < 1){
+			alert("已经是最后一台设备");
+			return;
+		}
+		lc1++;
+		rc1--;
+		$window1.animate({left:'-=8.4rem'}, 500);
+		currentIndex++;
+		mac=mac_[currentIndex].trim();
+		note = note_[currentIndex];
+		type = type_[currentIndex];
+		console.log("right_type="+type);
+		passWord = password_[currentIndex].trim();
+		SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+NODE=1\r\n'));
+		lampBottomDisplay();//彩灯颜色滚动条和颜色选择块的显示控制
+	});
+	
+	/* 弹窗 */
+	$('#time_extend').click(function(){
+		if(canClick){
+			$(".box_input,.box,.extend_time").hide();//显示之前先全部隐藏
+			showExtend("alert");//延时弹窗显示
+			console.log("type_exten="+type);
+			if(type=="0"){
+				SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+TIMESHUT=1?\r\n'));
+			}else if(type=="W05"){
+				SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+CLTIMESHUT=1?\r\n'));
+			}
+		}else{
+			alert("该设备离线");
+		}
+	});
+	
+	//定时跳转
+	$('#time_set').click(function(){
+		if(canClick){
+			console.log("type="+type);
+			window.location.href="http://www.shuworks.com/HFQL/smart/timer.php?mac="+mac+"&passWord="+passWord+"&type="+type;
+		}else{
+			alert("该设备离线");
+		}
+	});
+	
+	//关闭延时
+	$('#extend_cancel').click(function(){
+		$('#mybg,#alert,.extend_time').hide();
+		if(type=="0"){
+			SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+TIMESHUT=1,2,0\r\n'));
+		}else if(type=="W05"){
+			SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+CLTIMESHUT=1,2,0\r\n'));
+		}
+	});
+	
+	//恢复出厂设置点击
+	$('#reset').click(function(e){
+		e.stopPropagation();
+		if(canClick){
+			HandleOnClose();
+		}else{
+			alert("该设备离线");
+		}
+	});
+	
+	//修改密码
+	$('#change_psw').click(function(e){
+		e.stopPropagation();
+		if(!canClick){
+			alert("该设备离线");
+			return;
+		}
+		var r=window.confirm("设置本地设备密码？");
+		if (r==true){
+			editpass = prompt("输入新的密码");
+			if(editpass.length != 6){
+				alert("设置密码长度必须为6位");
+			}else{
+				password_[index_] = editpass;
+				passWord = editpass;
+				var xmlhttp;
+				if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+					xmlhttp=new XMLHttpRequest();
+				}else{// code for IE6, IE5
+					xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+				}
+				xmlhttp.onreadystatechange=function(){
+					if (xmlhttp.readyState==4 && xmlhttp.status==200){
+						alert(xmlhttp.responseText);
+					}
+				}
+				var url = "device.info/password.php";
+				url = url + "?mac=" + mac_[index_];
+				url = url + "&openid=" + openid;
+				url = url + "&pwd=" + passWord;
+				xmlhttp.open("POST",url,true);
+				xmlhttp.send();	
+			} 
+		}else{
+			var r1=window.confirm("设置远程设备密码？");
+			if (r1==true){
+				editpass = prompt("输入新的密码");
+				if(editpass.length != 6){
+					alert("设置密码长度必须为6位");
+				}else{
+					SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+EDITPWD='+ editpass +'\r\n'));
+				}
+			}else{
+			}
+		}
+	});	
+	//固件更新
+//	$('#update').click(function(e){
+//		e.stopPropagation();
+////		SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+V'+'\r\n'));
+//		$.ajax({  
+////          url:"jq_readXml.jsp?rnum="+<%=num%>,  
+//			url:"http://home.huafanyq.com:8550/home/wifi2/firmware/HF-SP2-WiFiPlug/version.xml",  
+//          type:"GET",  
+//          dataType:"JSONP",  
+//          timeout: 5000,  
+//          error: function(xml){  
+//              alert('Error loading XML document'+xml);  
+//          },  
+//          success: function(xml){  
+//          	alert(xml);
+////              $(xml).find("student").each(function(i){  
+////                  var id=$(this).children("id");   //取对象  
+////                  var id_value=$(this).children("id").text();  //取文本 或者 $("id" , xml).text();   
+////                  var name_value=$(this).children("name").text();  
+////                  //alert(id_value);//这里就是ID的值了。  
+////                  //alert($(this).attr("email")); //这里能显示student下的email属性。  
+////                  $('<li></li>').html(id_value+"&nbsp;&nbsp;&nbsp;"+name_value+"&nbsp;&nbsp;&nbsp;"+$(this).attr("email")).appendTo('ol');  
+////              });  
+//          }  
+//      }); 
+//	});
+	//设置按钮点击
+	$('#select').click(settingClick);
+	
+	//延时弹窗点击ok
+	var delay_switch=1;
+	var delay_long=0;
+	$('#time_extend_ok').click(function(){
+		if($('#check_1').attr("checked")){
+			delay_switch=1;
+		}else{
+			delay_switch=0;
+		}
+		delay_long=$('#time_extend_input').val();
+		if(delay_long!=""){
+			if(type=="0"){
+				console.log("chazuodianji");
+				SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+TIMESHUT=1,' + delay_switch + ',' + delay_long + '\r\n'));
+			}else if(type=="W05"){
+				console.log("caidengdianji");
+				SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+CLTIMESHUT=1,' + delay_switch + ',' + delay_long + '\r\n'));
+			}
+		}
+		$('#mybg,#alert,.box,.box_input').hide();
+	});
+	
+	//back键返回
+	$('#back').click(function(){
+		window.location.href="http://www.shuworks.com/HFQL/smart/main.php";
+	});
+	
+	//更改名称
+	$('.name').click(function(e){
+		$('.name').css("display","none");
+		$('.edit').css("display","");
+		$('.edit').val(note);
+		$('#edit'+currentIndex).focus();
+		$('.bottom').css("display","none");
+		$('.setting_menu').css("display","none");
+	});
+	$('.edit').blur(function(){
+		$('.name').css("display","");
+		$('.edit').css("display","none");
+		$('.bottom').css("display","");
+		$('.setting_menu').css("display","");
+		note = this.value;
+		if(this.value==""){
+			note = "空";
+		}
+		note_[currentIndex] = this.value;
+		$('.name').html(note);
+	});
+	$(document).click(function(){
+	    $('.setting_menu .div_img_setting_then').css('display','none');
+	    $("#shake").attr('src',"imgs/shake_before.png"); 
+	   	//取消摇一摇监听
+	   	stopShake();
+	});
+});
+
+/***************************************以下为子函数部分*******************************************/
+function addDeviceList(obj) {
+	for(var j=0;j<type_.length;j++){
+		console.log("type"+j+";"+type_[j]);
+	}
+	var div_out = document.getElementById(obj);
+	for(var i=0;i<count;i++){
+		switch(type_[i]){
+			case "0":
+				var div = document.createElement("div");
+				div.setAttribute("class","apply_array");
+				var div_in = document.createElement("div");
+				div_in.setAttribute("class","apply_img");
+				var img = document.createElement("img");
+				img.setAttribute("src","imgs/plug.png");
+				var label = document.createElement("label");
+				label.setAttribute("class","name");
+				label.innerHTML = noteList[i];
+				var edit = document.createElement("input");
+				edit.setAttribute("class","edit");
+				edit.setAttribute("id","edit"+i);
+				div_in.appendChild(img);
+				div.appendChild(div_in);
+				div.appendChild(edit);
+				div.appendChild(label);
+			break;
+			case "W05":
+				var div = document.createElement("div");
+				div.setAttribute("class","apply_array");
+			break;
+			case "strip":
+			
+			break;
+		}
+		div_out.appendChild(div);
+	}
+}
+
+//显示延时弹窗
+function showExtend(Str){
+	var myAlert = document.getElementById(Str); 
+	myAlert.style.display = "block";  
+	myAlert.style.position = "absolute";  
+	myAlert.style.top = "50%";  
+	myAlert.style.left = "50%";  
+	myAlert.style.marginTop = (-myAlert.offsetHeight/2)+"px"; 
+	myAlert.style.marginLeft = (-myAlert.offsetWidth/2)+"px"; 
+	$('#mybg').show();
+	$('#mybg').click(function(){
+		$('#mybg').hide();
+		$('#'+Str).hide();
+	});
+}
+//恢复出厂设置
+function HandleOnClose() {
+	  var close = confirm("确定恢复出厂设置?");
+	  if (close) {
+		SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+DFT\r\n'));
+		alert('设备已重置，请重新配置！');
+		$('.name').html("设备离线");
+		$('.name').css('color','red');
+		$("#No"+currentIndex).attr('src',"imgs/plug_off.png"); 
+		canClick=false;
+	  }
+	  else{
+		window.event;
+	  }
+	  return message;
+}
+//警告
+function AlertDeviceState(){
+	$('.name').html("设备离线");
+	$('.name').css('color','red');
+	$("#No"+currentIndex).attr('src',"imgs/plug_off.png"); 
+	canClick=false;
+}
+
+//发送数据
+function SendData(Str) {
+	if (ws.readyState == 1)
+		try {
+			var temp = "" + Str;
+			var reg = /\s/g;
+			var output = temp.replace(reg, "");
+			ws.send(output);
+			console.log('SEND:' + output);
+		} catch (ex) {
+			alert(ex.message);
+		}
+}
+
+//接收数据
+function RecieveToggle(Str){
+	if(Str){
+		$("#No"+currentIndex).attr('src',"imgs/plug.png"); 
+		state = true;
+	}else{
+		$("#No"+currentIndex).attr('src',"imgs/plug_off.png"); 
+		state = false;
+	}
+}
+
+function touchclick(str){
+	switch(str){
+		case "power_protect":
+			$("#power_protect").attr("src","imgs/power_protect.png");
+			setTimeout(function(){$("#power_protect").attr("src","imgs/power_protect_before.png");},300);
+		break;
+		case "time_set":
+			$("#time_set").attr("src","imgs/time_set.png");
+			setTimeout(function(){$("#time_set").attr("src","imgs/time_set_before.png");},300);
+		break;
+		case "time_extend":
+			$("#time_extend").attr("src","imgs/time_extend.png");
+			setTimeout(function(){$("#time_extend").attr("src","imgs/time_extend_before.png");},300);
+		break;
+		case "img_r":
+			$(".apply .img_r").attr("src","imgs/right.png");
+			setTimeout(function(){$(".apply .img_r").attr("src","imgs/right_before.png");},300);
+		break;
+		case "img_l":
+			$(".apply .img_l").attr("src","imgs/left.png");
+			setTimeout(function(){$(".apply .img_l").attr("src","imgs/left_before.png");},300);
+		break;
+	}
+}
+
+//两位数
+function padLeft(str){ 
+	if(str >= 10) 
+		return str; 
+	else 
+		return "0" +str; 
+} 
+
+function settingClick(e){
+	e.stopPropagation();
+	if(settingClickFlag==1){
+		settingClickFlag=0;
+		$('.setting_menu .div_img_setting_then').css('display','');
+	}else{
+		settingClickFlag=1;
+		$('.setting_menu .div_img_setting_then').css('display','none');
+	}
+}
+
+//彩灯颜色滚动条和选择块的显示控制
+function lampBottomDisplay(){
+	//若当前为彩灯，则显示颜色滚动条和颜色选择块
+	console.log("type_l="+type);
+	if(type=="W05"){
+		$('#bright-div,#color-picker').show();
+		$('.div_img_setting_first').hide();
+		setTimeout("delayShow()",300);
+		$('.singleFlash').show();
+		$('.sevenFlash').show();
+		$('.shake').show();
+		$('.power_protect').hide();
+		SendData(Encrypt('CONTROL ' + mac + ' P ' + passWord + ' AT+CLNODE=1\r\n'));
+	}else if(type=="0"){
+		$('#bright-div,#color-picker').hide();
+		$('.div_img_setting_first').show();
+		$('.canvas-div').hide();
+		$('.sevenFlash').hide();
+		$('.singleFlash').hide();
+		$('.shake').hide();
+		$('.power_protect').show();
+	}
+}
+
+//延时显示用到的函数
+function delayShow(){
+	$('.canvas-div').show();
+}
---
+++ smartDevice/page2_.php
@@ -0,0 +1,270 @@
+<?php
+	error_reporting(0);
+//	if (isset($_GET['openid'])){
+//	    $openid = $_GET['openid']; 
+//	}
+	$openid="ozcT3wn5WskYwQINmFlJYcMdbkZs";
+//	if (isset($_GET['sequence'])){
+//	    $sequence = $_GET['sequence']; 
+//	}
+	$sequence=0;
+	$conn = mysql_connect("localhost","root", "qinlu126"); 
+	if (!$conn){
+		die("连接数据库失败：" . mysql_error());
+	}
+	mysql_select_db("smart_outlet", $conn)
+	or die("dadatbase error!");
+	mysql_query("set character set UTF8");
+	mysql_query("SET NAMES UTF8");
+	$check_query = mysql_query("select * from device_list where openid='$openid' ORDER BY id DESC limit 6");
+	$index=0;
+	$mac[$index]="549A11C01174";
+	$note[$index]="123";
+	$password[$index]='888888';
+	$type[$index]='0';
+	$index++;
+	$mac[$index]="549A11C0017F";
+	$note[$index]="cz";
+	$password[$index]='888888';
+	$type[$index]='W04';
+	$index++;
+//	while($result = mysql_fetch_array($check_query))
+//	{
+//		if($index==1){
+//			$mac[$index]="549A11C0017F";
+//			$note[$index]="cz";
+//			$password[$index]='888888';
+//			$type[$index]='W04';
+//		}else{
+//			$mac[$index] = $result['mac'];
+//			$note[$index] = $result['note'];
+//			$password[$index] = $result['pwd'];
+//			$type[$index] = $result['device'];
+//			if($result['pwd'] == ""){
+//				$password[$index] = '888888';
+//			}
+//			if($result['device'] == ""){
+//				$type[$index] = '0';
+//			}
+//			$index++;
+//		}
+//	}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8"> 
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>华凡秦鲁智能家居</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="renderer" content="webkit">
+<meta http-equiv="Cache-Control" content="no-siteapp" />
+<script src="js/jquery.1.7.2.min.js"></script>	
+<script src="js/aes.js"></script>
+<script src="js/md5.js"></script>
+<script src="js/pad-zeropadding.js"></script>
+<script src="js/encode.js"></script>
+<script type="text/javascript">
+		document.getElementsByTagName('html')[0].style.fontSize=document.documentElement.clientWidth / 12 +"px";
+</script>
+<script type="text/javascript" src="js/websocket_.js"></script>
+<script src="js/shan_.js" type="text/javascript"></script>
+<link href="css/shan_.css" rel="stylesheet" type="text/css">
+<script type="text/javascript">
+	//控制部分变量
+	var noteList = <?php echo json_encode($note);?>;
+	var count = <?php echo $index ?>;
+	var note = '<?php echo $note[0];?>';
+	var mac = '<?php echo $mac[0];?>'.trim();
+	var type = '<?php echo $type[0];?>'.trim();
+	var passWord = '<?php echo $password[0];?>'.trim();
+	var note_ = <?php echo json_encode($note);?>;
+	var mac_ = <?php echo json_encode($mac);?>;	
+	var password_ = <?php echo json_encode($password);?>;
+	var type_ = <?php echo json_encode($type);?>;
+	var currentIndex = <?php echo $sequence ?>;
+	var openid = '<?php echo $openid ?>';
+</script>
+</head>
+
+<body>
+<!--头部分-->
+<div class="header" >
+	<div class="mode_title">当前模式</div>
+	<div class="mode_content">远程</div>
+	<div class="back">
+		<img src="imgs/back.png" />
+	</div>
+</div>
+
+<!--滑动插座部分-->
+<div class="apply">
+	<div class="img_l" onclick="touchclick('img_l')"><img src="imgs/left_before.png" /></div>
+	<div class="apply_nav">
+		<div class="apply_w " id="apply_w">
+		</div>
+	</div>
+	<div class="img_r" onclick="touchclick('img_r')"><img src="imgs/right_before.png"/></div>
+</div>
+
+<!--彩灯绝对布局-->
+<div class="canvas-div">
+	<img class="switch-lamp" src="imgs/lampOn.png"/>
+	<canvas id="can"></canvas>
+</div>
+<div id="mybg"></div>
+
+<!--延时输入框部分-->
+<div id="alert" class="alert">  
+	<div class="diag_header">
+		延时设置
+	</div>
+	<div class="diag_content">
+		<!--设置延时时间-->
+		<div class="box_input">
+			<input type="text" class="time_input" id="time_extend_input" value="1"/>
+			<label for="time_extend_input" class="min">Min</label>
+		</div>
+		<div class="box">
+			<section class="box1">
+				<input id="check_1" class="checkbox" name="check" type="checkbox" checked>
+				<label for="check_1" class="trigger"></label>
+			</section>
+			<section class="box2">
+				<img class="button_extend" id="time_extend_ok" src="imgs/extend_ok.png"/>
+			</section>
+		</div>
+		<!--剩余延时时间-->
+		<div class="extend_time">
+			<label class="extend_time_text">Min</label>
+			<img id="extend_cancel" class="extend_cancel" src="imgs/extend_cancel.png"/>
+		</div>
+	</div>
+</div> 
+
+<!--华丽的分隔符-->
+<div class="clear" ></div> 
+
+<!--颜色滚动条-->
+<div class="bright-div" id="bright-div">
+	<div class="slide-bar" id="slide-bar">
+		<div class="slide-btn" id="slide-btn"></div>
+		<div class="slide-progress" id="slide-progress"></div>
+	</div>
+</div>
+
+<!--颜色选择块-->
+<ul class="color-picker cl" id="color-picker">
+	<li class="color-li normalLi red" data-value = "FC2429" ></li>
+	<li class="color-li normalLi green" data-value = "3DFF47" ></li>
+	<li class="color-li normalLi pure" data-value = "2023FC" ></li>
+	<li class="color-li normalLi yellow" data-value = "FFFD27" ></li>
+	<li class="color-li normalLi pink" data-value = "FE1F25" ></li>
+	<li class="color-li normalLi white" data-value = "FFFFFF" ></li>
+	<li class="color-li normalLi blue" data-value = "11FEFE" ></li>
+</ul>
+
+<!--单色闪烁弹窗-->
+<div id="singleAlert" class="alert">  
+	<div class="diag_header">
+		单色闪烁
+	</div>
+	<div class="diag_content single_content">
+		<!--颜色滚动条-->
+		<div class="bright-div  bright-div-speed">
+			<div class="slide-bar" id="bar-single-speed">
+				<div class="slide-btn" id="btn-single-speed"></div>
+				<div class="slide-progress" id="pro-single-speed"></div>
+			</div>
+		</div>
+		<div class="bright-div">
+			<div class="slide-bar" id="bar-single-light">
+				<div class="slide-btn slide-btn-light" id="btn-single-light"></div>
+				<div class="slide-progress" id="pro-single-light"></div>
+			</div>
+		</div>
+		<!--颜色选择块-->
+		<ul class="color-picker cl">
+			<li class="color-li singleLi red color-picker-alert" data-value = "FC2429" id="singleLi"></li>
+			<li class="color-li singleLi green color-picker-alert" data-value = "3DFF47" id="singleLi"></li>
+			<li class="color-li singleLi pure color-picker-alert" data-value = "2023FC" id="singleLi"></li>
+			<li class="color-li singleLi yellow color-picker-alert" data-value = "FFFD27" id="singleLi"></li>
+			<li class="color-li singleLi pink color-picker-alert" data-value = "FE1F25" id="singleLi"></li>
+			<li class="color-li singleLi white color-picker-alert" data-value = "FFFFFF" id="singleLi"></li>
+			<li class="color-li singleLi blue color-picker-alert" data-value = "11FEFE" id="singleLi"></li>
+		</ul>
+		<!--完成按钮-->
+		<img class="single_cancel" id="single_finish" src="imgs/finish.png"/>
+	</div>
+</div> 
+
+<!--七色闪烁弹窗-->
+<div id="sevenAlert" class="alert">  
+	<div class="diag_header">
+		七色闪烁
+	</div>
+	<div class="diag_content seven_content">
+		<!--颜色滚动条-->
+		<div class="bright-div bright-div-speed">
+			<div class="slide-bar" id="bar-seven-speed">
+				<div class="slide-btn" id="btn-seven-speed"></div>
+				<div class="slide-progress" id="pro-seven-speed"></div>
+			</div>
+		</div>
+		<div class="bright-div">
+			<div class="slide-bar" id="bar-seven-light">
+				<div class="slide-btn slide-btn-light" id="btn-seven-light"></div>
+				<div class="slide-progress" id="pro-seven-light"></div>
+			</div>
+		</div>
+		<!--完成按钮-->
+		<img class="seven_cancel" id="seven_finish" src="imgs/finish.png"/>
+	</div>
+</div> 
+
+<!--设置菜单部分-->
+<div class="setting_menu">   
+	<div class="div_img_setting div_img_setting_first" id="select">
+		<img id="setting" src="imgs/setting_before.png"/>
+	</div>
+	<div class="div_img_setting div_img_setting_then div_img_setting_second" >
+		<img id="reset" src="imgs/reset_before.png"/>
+	</div>
+	<div class="div_img_setting div_img_setting_then">
+		<img id="update" src="imgs/update_before.png"/>
+	</div>
+	<div class="div_img_setting div_img_setting_then">
+		<img id="change_psw" src="imgs/change_psw_before.png"/>
+	</div>
+</div>
+
+<!--底部菜单部分-->
+<div class="bottom">
+	<div class="div_img_bot" >
+		<img id="time_extend" src="imgs/time_extend_before.png" />
+	</div>
+	<div class="div_img_bot" >
+		<img id="time_set" src="imgs/time_set_before.png" />
+	</div>
+	<div class="div_img_bot shake">
+		<img id="shake" src="imgs/shake_before.png" />
+	</div>
+	<div class="div_img_bot singleFlash">
+		<img id="singleFlash" src="imgs/singleFlash_before.png" />
+	</div>
+	<div class="div_img_bot sevenFlash">
+		<img id="sevenFlash" src="imgs/sevenFlash_before.png" />
+	</div>
+	<div class="div_img_bot power_protect">
+		<img id="power_protect" src="imgs/power_protect_before.png" />
+	</div>
+</div>
+
+<!--彩灯JavaScript-->
+<script type="text/javascript" src="js/easeljs-NEXT.min.js"></script>
+<script type="text/javascript" src="js/lamp.js"></script>
+
+</body>
+</html>
