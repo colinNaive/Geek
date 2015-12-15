e314083bed99733dab4ef50f265d8bd0eaacd6ed
colinNaive <colinNaive@shu.edu.cn>
2015-12-15 19:49:38 +0800

    yueba_nodejs

---
+++ yueba/models/user.js
@@ -0,0 +1,125 @@
+var mongodb = require('./db');
+var crypto = require('crypto');
+
+function User(user) {
+  this.name = user.name;
+  this.password = user.password;
+  this.email = user.email;
+};
+
+module.exports = User;
+
+//存储用户信息
+User.prototype.save = function(callback) {
+  var md5 = crypto.createHash('md5'),
+      email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
+      head = "http://www.shqinlu.com/" + email_MD5 + "?s=48";
+  //要存入数据库的用户信息文档
+  var user = {
+      name: this.name,
+      password: this.password,
+      email: this.email,
+      head: head
+  };
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);//错误，返回 err 信息
+    }
+    //读取 users 集合
+    db.collection('users', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);//错误，返回 err 信息
+      }
+      //将用户数据插入 users 集合
+      collection.insert(user, {
+        safe: true
+      }, function (err, user) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, user[0]);//成功！err 为 null，并返回存储后的用户文档
+      });
+    });
+  });
+};
+
+//读取用户信息
+User.get = function(name, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);//错误，返回 err 信息
+    }
+    //读取 users 集合
+    db.collection('users', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);//错误，返回 err 信息
+      }
+      //查找用户名（name键）值为 name 一个文档
+      collection.findOne({
+        name: name
+      }, function (err, user) {
+        mongodb.close();
+        if (err) {
+          return callback(err);//失败！返回 err
+        }
+        callback(null, user);//成功！返回查询的用户信息
+      });
+    });
+  });
+};
+
+
+//读取用户信息
+User.getAll = function(callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);//错误，返回 err 信息
+    }
+    //读取 users 集合
+    db.collection('users', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);//错误，返回 err 信息
+      }
+      //查找用户名（name键）值为 name 一个文档
+      collection.find(function (err, user) {
+        mongodb.close();
+        if (err) {
+          return callback(err);//失败！返回 err
+        }
+        callback(null, user);//成功！返回查询的用户信息
+      });
+    });
+  });
+};
+
+//返回所有用户成员信息
+User.getMembers = function(callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 users 集合
+    db.collection('users', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //返回只包含 name、time、title 属性的文档组成的存档数组
+      collection.find().toArray(function (err, members) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, members);
+      });
+    });
+  });
+};
